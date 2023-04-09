bfc24790e1c1c3decb649f61e9d2faf70b9eba8c
Priyajit27 <priyajitpaul27@gmail.com>
2023-04-08 21:37:42 -0800

    navbar

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,39 +1,82 @@
-import  { useState } from "react";
-import { Link } from "react-router-dom";
-import MenuIcon from '@mui/icons-material/Menu';
-import './Navbar.css'
-import React from 'react';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import { useTheme } from "@mui/material";
+import * as Palette from "../../configs/pallete";
+// import { gr }
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  CssBaseline,
+  Button,
+} from "@mui/material";
+import Logo from "../../assets/logo.svg";
+import "./Navbar.css";
+import React from "react";
+
 function Navbar() {
-  const [isMobile,setIsMobile]=useState("false");
+  const allNavLinks = [
+    { name: "Home", link: "/" },
+    { name: "About", link: "/about" },
+    { name: "Contact", link: "/" },
+    { name: "Help", link: "/" },
+  ];
+  const [isActive, setIsActive] = React.useState("Home");
+  const [isIconClicked, setIsIconClicked] = React.useState(false);
+  const navigate = useNavigate();
+  const theme = useTheme();
+  const [isNotMobile, setIsMobile] = useState(theme.breakpoints.down("sm"));
+  // const isMobile = theme.breakpoints.down('sm')
+
   return (
-    <div className="hello">
-    <nav className="navbar" >
-      <h2 className="logo">Agro-buddy</h2>
-      <ul className={(isMobile=="false")?"nav-links":"nav-links-mobile"} >
-      {/* <ul  className="nav-links"> */}
-       <Link to ="/" className="home">
-       <li>Home</li>
-       </Link>
-       <Link to ="/about" className="home">
-       <li>About</li>
-       </Link>
-       <Link to ="/" className="home">
-       <li>Contact</li>
-       </Link>
-       <Link to ="/" className="home">
-       <li>Help</li>
-       </Link>
-       <Link to ="/" className="home">
-       <li>Home</li>
-       </Link>
-      </ul>
-      <button className="mobileview" onClick={()=>setIsMobile(!isMobile)}>{
-      isMobile?( <MenuIcon />)
-        :(<MenuIcon />)}
-    </button>
-    </nav>
-    </div>
-  )
+    <AppBar position="static" elevation={0}>
+      <CssBaseline />
+      <Toolbar variant="regular">
+        <div variant="h5" fontWeight="bold" className="logo">
+          <img
+            style={{ height: "26px", width: "175px" }}
+            src={Logo}
+            alt="AgroBuddy"
+          />
+        </div>
+        <div className={isNotMobile ? "nav-links" : `nav-links-mobile ${isIconClicked && "icon-active"}`}>
+          {allNavLinks.map((link) => (
+            <li
+              key={link.name}
+              className={isNotMobile ? "nav-link" : `nav-link-mobile`}
+              onClick={() => {
+                setIsActive(link.name);
+                navigate(link.link);
+              }}
+            >
+              <Typography>{link.name}</Typography>
+              <div
+                style={
+                  isActive === link.name ? { width: "2.5vw" } : { width: 0 }
+                }
+                className="underline"
+              ></div>
+            </li>
+          ))}
+        </div>
+        <div
+          className="mobileview"
+          onClick={() => {
+            setIsMobile((prev) => !prev);
+            setIsIconClicked(!isIconClicked);
+          }}
+        >
+          {isNotMobile ? (
+            <MenuIcon color="secondary" />
+          ) : (
+            <CloseIcon color="secondary" />
+          )}
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
-export default Navbar
+export default Navbar;
