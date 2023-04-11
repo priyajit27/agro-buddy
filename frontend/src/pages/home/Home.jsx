ade5444d1aebdf29e9f1d1f15954657feb4852ce
Priyajit27 <priyajitpaul27@gmail.com>
2023-04-10 20:16:45 -0800

    Graph added

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -7,12 +7,14 @@ import Hero from './components/Hero/Hero';
 import SignIn from '../../components/forms/SignIn';
 import Footer from '../../components/footer/Footer';
 import SignUp from '../../components/forms/SignUp';
+import Content from '../../components/content/Content'
 
 
 function Home() {
   return (
     <div>
       <Hero/>
+      <Content/>
     </div>
   )
 }
